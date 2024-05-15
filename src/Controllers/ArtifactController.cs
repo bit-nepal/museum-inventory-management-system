1ee0eb752ced22479b3ae1df78ef5104da94bbb3
manzilstha07 <manzilstha12@gmail.com>
2024-05-15 14:38:37 +0545

    Create file created

---
+++ src/Controllers/ArtifactController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mims.Data;
+using mims.Models;
+
+namespace mims.Controllers
+{
+    public class ArtifactController : Controller
+    {
+        private readonly ArtifactContext _artifactContext;
+        public ArtifactController(ArtifactContext artifactContext)
+        {
+            _artifactContext = artifactContext;
+        }
+
+
+    }
+}
