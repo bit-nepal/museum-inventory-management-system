8b5b11882f40c853fed0a24ce6f1b6a28b028c77
Amrit Paudel <the_amrit@proton.me>
2024-05-18 18:00:37 +0545

    feat: Ui improvement to Artifact List

--- src/Controllers/ArtifactController.cs
+++ src/Controllers/ArtifactController.cs
@@ -2,28 +2,35 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace mims.Controllers;
 
-[Route("Admin/Artifact")]
+[Route("Admin/Artifacts")]
 public class ArtifactController : Controller
 {
-    private readonly ILogger<ArtifactController> _logger;
-    private const string rootViewDirectory = "/Views/Admin/Artifact/";
-    public ArtifactController(ILogger<ArtifactController> logger)
-    {
-        _logger = logger;
-    }
+  private readonly ILogger<ArtifactController> _logger;
+  private const string rootViewDirectory = "/Views/Admin/Artifact/";
+  public ArtifactController(ILogger<ArtifactController> logger)
+  {
+    _logger = logger;
+  }
 
-    public IActionResult Index()
-    {
-        return View(rootViewDirectory + "Index.cshtml");
-    }
-    [Route("Create")]
-    public IActionResult Create()
-    {
-        return View(rootViewDirectory + "Create.cshtml");
-    }
-    [Route("Update")]
-    public IActionResult Update()
-    {
-        return View(rootViewDirectory + "Update.cshtml");
-    }
+  public IActionResult Index()
+  {
+    return View(rootViewDirectory + "Index.cshtml");
+  }
+  [Route("Create")]
+  public IActionResult Create()
+  {
+    return View(rootViewDirectory + "Create.cshtml");
+  }
+  [Route("Update")]
+  public IActionResult Update()
+  {
+    return View(rootViewDirectory + "Update.cshtml");
+  }
+
+  [HttpGet("{ArtifactId}")]
+  public IActionResult GetTenantById(int TenantId)
+  {
+    return View(rootViewDirectory + "Details.cshtml");
+  }
+
 }
