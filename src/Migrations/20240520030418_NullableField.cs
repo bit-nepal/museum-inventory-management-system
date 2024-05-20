fd82616da2cf179fb44bc1cf4c003bdaa82cdb4e
Amrit Paudel <the_amrit@proton.me>
2024-05-20 08:55:06 +0545

    feat: Implemented Add Artifact

---
+++ src/Migrations/20240520030418_NullableField.cs
@@ -0,0 +1,494 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace mims.Migrations
+{
+    /// <inheritdoc />
+    public partial class NullableField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "weight",
+                table: "Artifacts",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "used_matriel",
+                table: "Artifacts",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "signature_of_receiver",
+                table: "Artifacts",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "remarks",
+                table: "Artifacts",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "place_of_receipt",
+                table: "Artifacts",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "place_of_origin",
+                table: "Artifacts",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "physical_condition",
+                table: "Artifacts",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "photo_no",
+                table: "Artifacts",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "period",
+                table: "Artifacts",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "number_of_object",
+                table: "Artifacts",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name_of_donor",
+                table: "Artifacts",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name_of_artist",
+                table: "Artifacts",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "measurement",
+                table: "Artifacts",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "location",
+                table: "Artifacts",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "inscription",
+                table: "Artifacts",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "estimated_value",
+                table: "Artifacts",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "date_of_aquisition",
+                table: "Artifacts",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Artifacts",
+                keyColumn: "weight",
+                keyValue: null,
+                column: "weight",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "weight",
+                table: "Artifacts",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Artifacts",
+                keyColumn: "used_matriel",
+                keyValue: null,
+                column: "used_matriel",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "used_matriel",
+                table: "Artifacts",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Artifacts",
+                keyColumn: "signature_of_receiver",
+                keyValue: null,
+                column: "signature_of_receiver",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "signature_of_receiver",
+                table: "Artifacts",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Artifacts",
+                keyColumn: "remarks",
+                keyValue: null,
+                column: "remarks",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "remarks",
+                table: "Artifacts",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Artifacts",
+                keyColumn: "place_of_receipt",
+                keyValue: null,
+                column: "place_of_receipt",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "place_of_receipt",
+                table: "Artifacts",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Artifacts",
+                keyColumn: "place_of_origin",
+                keyValue: null,
+                column: "place_of_origin",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "place_of_origin",
+                table: "Artifacts",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Artifacts",
+                keyColumn: "physical_condition",
+                keyValue: null,
+                column: "physical_condition",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "physical_condition",
+                table: "Artifacts",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Artifacts",
+                keyColumn: "photo_no",
+                keyValue: null,
+                column: "photo_no",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "photo_no",
+                table: "Artifacts",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Artifacts",
+                keyColumn: "period",
+                keyValue: null,
+                column: "period",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "period",
+                table: "Artifacts",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Artifacts",
+                keyColumn: "number_of_object",
+                keyValue: null,
+                column: "number_of_object",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "number_of_object",
+                table: "Artifacts",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Artifacts",
+                keyColumn: "name_of_donor",
+                keyValue: null,
+                column: "name_of_donor",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name_of_donor",
+                table: "Artifacts",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Artifacts",
+                keyColumn: "name_of_artist",
+                keyValue: null,
+                column: "name_of_artist",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name_of_artist",
+                table: "Artifacts",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Artifacts",
+                keyColumn: "measurement",
+                keyValue: null,
+                column: "measurement",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "measurement",
+                table: "Artifacts",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Artifacts",
+                keyColumn: "location",
+                keyValue: null,
+                column: "location",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "location",
+                table: "Artifacts",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Artifacts",
+                keyColumn: "inscription",
+                keyValue: null,
+                column: "inscription",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "inscription",
+                table: "Artifacts",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Artifacts",
+                keyColumn: "estimated_value",
+                keyValue: null,
+                column: "estimated_value",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "estimated_value",
+                table: "Artifacts",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Artifacts",
+                keyColumn: "date_of_aquisition",
+                keyValue: null,
+                column: "date_of_aquisition",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "date_of_aquisition",
+                table: "Artifacts",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
