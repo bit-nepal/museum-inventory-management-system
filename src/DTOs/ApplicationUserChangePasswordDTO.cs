bf370fb09ce0c35dec36490b9bf5b4a3961300fb
manzilstha07 <manzilstha12@gmail.com>
2024-06-17 19:28:04 +0545

    username login password

--- src/DTOs/ApplicationUserChangePasswordDTO.cs
+++ src/DTOs/ApplicationUserChangePasswordDTO.cs
@@ -1,7 +1,7 @@
 namespace mims.DTOs;
 public class ApplicationUserChangePasswordDTO
 {
-  public string EmailOrUserName { get; set; } = null!;
   public string OldPassword { get; set; } = null!;
   public string NewPassword { get; set; } = null!;
+  public string ConfirmNewPassword { get; set; } = null!;
 }
---
+++ src/DTOs/ApplicationUserChangeUsernameDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace mims.DTOs
+{
+    public class ApplicationUserChangeUsernameDTO
+    {
+        public string NewUserName { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
